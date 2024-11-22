73095c6a0f7c774b734d50d8568d461abed08c57
storresp <santiagotopo@unisabana.edu.co>
2024-11-21 23:41:23 -0500

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Start yarn"
+yarn start
+
 echo "generating application key..."
 php artisan key:generate --show
 
